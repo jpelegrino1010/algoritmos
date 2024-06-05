a5e4e245c670f2dbe57fccc4c4dc738e67911490
jpelegrino1010 <julio.pelegrino@viseo.com>
2024-06-05 16:11:20 -0400

    Sales by match

---
+++ algoritms-java/src/main/java/salebymatch/Solution.java
@@ -0,0 +1,36 @@
+package salebymatch;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static void main(String[] args) {
+        int arr []={10,20,20,10,10,30,50,10,20};
+        System.out.println(sockMerchant(9,arr));
+    }
+
+    static int sockMerchant(int n, int[] ar) {
+//        1) initialize a map to store the pairs
+        Map<Integer, Integer> colorsCount = new HashMap<>();
+
+//        2)Loop through the array
+        for (int i = 0; i < ar.length; i++) {
+//            3) if the current value not found in map put a 1 into the map
+//            3) if not add 1 to existing key
+            if (colorsCount.get(ar[i]) == null) {
+                colorsCount.put(ar[i], 1);
+            } else {
+                colorsCount.put(ar[i], colorsCount.get(ar[i]) + 1);
+            }
+        }
+//        4) initialize an integer to 0
+        int pairs = 0;
+//        5) Loop through the map
+        for (Map.Entry<Integer, Integer> entry : colorsCount.entrySet()) {
+//            6) sum pair value + current value divide by 2
+            pairs += entry.getValue() / 2;
+        }
+//        7) return the sum of pairs
+        return pairs;
+    }
+}
