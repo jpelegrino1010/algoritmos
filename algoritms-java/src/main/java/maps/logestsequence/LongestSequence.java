54b2edc9a9927f193290bdece9e022e3175e3244
jpelegrino1010 <julio.pelegrino@viseo.com>
2024-06-09 12:23:32 -0400

    refactor long sequence

--- algoritms-java/src/main/java/maps/logestsequence/LongestSequence.java
+++ algoritms-java/src/main/java/maps/logestsequence/LongestSequence.java
@@ -10,6 +10,7 @@ public class LongestSequence {
 
     public static void main(String[] args) {
         System.out.println(longestSequence(new int[] {2,2,2,3,3,3,3,4,5,5,5,6,6,6}));
+        System.out.println(longestSequence2(new int[] {2,2,2,3,3,3,3,4,5,5,5,6,6,6}));
     }
 
     public static Map<Integer,Integer> longestSequence(int arr []) {
@@ -25,4 +26,24 @@ public class LongestSequence {
 
         return map;
     }
+
+    public static Map<Integer,Integer> longestSequence2(int arr []) {
+        int temp = arr[0];
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i<arr.length; i++) {
+            if(arr[i] == temp) {
+                if(map.containsKey(arr[i])) {
+                    map.put(arr[i], map.get(arr[i]) + 1);
+                }else {
+                    map.put(arr[i],1);
+                }
+            }else {
+                temp = arr[i];
+                map.put(arr[i],1);
+            }
+        }
+
+        return map;
+    }
+
 }
